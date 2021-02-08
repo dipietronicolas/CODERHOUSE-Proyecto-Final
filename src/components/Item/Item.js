540166527350ce33fa367cc914221c0a0500871d
Nicolas <dipietronicolas@gmail.com>
2021-02-08 13:24:20 -0300

    Pictures upload

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -4,14 +4,14 @@ import NumberFormat from 'react-number-format';
 
 export const Item = (props) => {
 
-
   return (
     <div className="Card">
       <img className="card-img-top" src={props.item.pageURLBig} alt="Card cap"></img>
+      
       <div className='Card-body px-2'>
         <div className="Card-header">
           <h5 className="Card-title">{props.item.title}</h5>
-          <p className="Card-paragraph">Precio: <NumberFormat value={props.item.price.toFixed(2)} displayType={'text'} thousandSeparator={true} prefix={'$'}/></p>
+          <p className="Card-paragraph">Precio: <NumberFormat value={props.item.price.toFixed(2)} displayType={'text'} thousandSeparator={true} prefix={'$'} /></p>
         </div>
         <Link
           to={`/item/${props.item.id}`}
